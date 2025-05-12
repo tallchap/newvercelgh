a631eed213ec272ca30dc279d1ac2d06c31d0d87
tallchap <orio195@yahoo.com>
2025-05-12 05:53:34 +0000

    homepage change

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
-    <main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <h1>vizard webhook page</h1>
+    <main
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <h1>vizard webhook</h1>
     </main>
   );
 }
